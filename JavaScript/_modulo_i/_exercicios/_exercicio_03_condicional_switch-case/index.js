2d35d69f764d826c993b23af86102b01a516cb73
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-04-19 13:55:32 -0300

    Criação do arquivo html e js do exercicio de condicional (switch case)

---
+++ JavaScript/_modulo_i/_exercicios/_exercicio_03_condicional_switch-case/index.js
@@ -0,0 +1,4 @@
+const valor = prompt("Digite um valor em metro");
+
+prompt("Escolha uma opção de conversão:\n 1) Milímetro (mm)\n 2) Centímetro (cm)\n 3) Decímetro (dm) (dam)\n 4) Decâmetro (dam)\n 5) Hectômetro (hm)\n 6) Quilômetro (km)")
+
