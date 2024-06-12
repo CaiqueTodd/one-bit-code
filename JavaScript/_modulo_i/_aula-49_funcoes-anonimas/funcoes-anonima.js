26dbd545d1cae3cb2f014a10a59d6f4d7b84bbc2
Caique <103804923+CaiqueTodd@users.noreply.github.com>
2024-06-11 23:35:20 -0300

    Funções recursivas

---
+++ JavaScript/_modulo_i/_aula-49_funcoes-anonimas/funcoes-anonima.js
@@ -0,0 +1,15 @@
+function somar(a, b) {
+    return a+b
+}
+
+const operacao = somar;
+
+console.log(operacao(2,3));
+
+
+let subtrair = function (a,b) {
+    return a - b;
+}
+
+console.log(subtrair(10,2));
+
